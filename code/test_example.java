b08c477a1b63ca66606d919cd7e9f0d1ccf008cf
Filip Klimeš <filip@filipklimes.cz>
2018-04-18 21:45:28 +0200

    somewhat good progress in chapter 6

---
+++ code/test_example.java
@@ -0,0 +1,24 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BusinessContextWeaverTest {
+
+    /* ... */
+
+    @Test
+    public void test() {
+        BusinessContextWeaver evaluator = new BusinessContextWeaver(createRegistry());
+        BusinessOperationContext context = new BusinessOperationContext("user.create");
+
+        context.setOutput(new User(
+            "John Doe",
+            "john.doe@example.com"
+        ));
+
+        evaluator.applyPostConditions(context);
+
+        User user = (User) context.getOutput();
+        Assert.assertEquals("John Doe", user.getName());
+        Assert.assertNull(user.getEmail());
+    }
+}
---
+++ code/test_example.py
@@ -0,0 +1,18 @@
+import unittest
+from business_context.identifier import Identifier
+
+
+class IdentifierTest(unittest.TestCase):
+    def test_split(self):
+        identifier = Identifier("auth", "loggedIn")
+        self.assertEqual("auth", identifier.prefix)
+        self.assertEqual("loggedIn", identifier.name)
+
+    def test_single(self):
+        identifier = Identifier("auth.loggedIn")
+        self.assertEqual("auth", identifier.prefix)
+        self.assertEqual("loggedIn", identifier.name)
+
+    def test_str(self):
+        identifier = Identifier("auth.loggedIn")
+        self.assertEqual('auth.loggedIn', identifier.__str__())
