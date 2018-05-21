957a9bd748ac01c7f91f7cf5956702ca5dcab366
Filip Klimeš <filip@filipklimes.cz>
2018-05-21 12:08:08 +0200

    chapter 6 final (maybe)

--- code/test_example.js
+++ code/test_example.js
@@ -5,21 +5,14 @@ chai.should();
 
 describe('IsNotNull', () => {
     describe('#interpret', () => {
-        it('evaluates if the argument is null', () => {
+        it('returns true if the argument is null', () => {
             const ctx = new BusinessOperationContext('user.create')
-            let expression = new IsNotNull(new Constant(
+            const expression = new IsNotNull(new Constant(
                 true,
                 ExpressionType.BOOL
             ))
-            let result = expression.interpret(ctx)
+            const result = expression.interpret(ctx)
             result.should.equal(true)
-
-            expression = new IsNotNull(new Constant(
-                null,
-                ExpressionType.VOID
-            ))
-            result = expression.interpret(ctx)
-            result.should.equal(false)
         })
     })
 })
